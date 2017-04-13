a1f8d29aa79e83a01d897e3ab3edb8ba0cef74eb
supaspha <sphamandla.mkhize@standardbank.co.za>
2017-04-13 09:26:42 +0200

    this file was created from the github website
    this file was created from the github website

---
+++ createdfromgithub.java
@@ -0,0 +1,13 @@
+public class createdfromgithub
+{
+public static void main(String args[])
+{
+  
+  for(int i = 5; i > 0;i--)
+  {
+  System.out.println(i);
+  }
+  System.out.print("Wishing you a safe flight back to India");
+}
+
+}
